86072a69552fc10af6c87802473a000bd642e5df
Lukas <lukas1033@gmail.com>
2020-04-03 08:58:12 +0100

    Added leaflet map

--- corona-stats/src/app/components/home/home.component.ts
+++ corona-stats/src/app/components/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { GetDataService } from 'src/app/services/get-data.service';
 import { CoronaStatsObject } from 'src/app/interfaces/CoronaStatsObject';
 
+// Leaflet maps - Has to be declared as 'L'
+declare let L;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -16,6 +19,12 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.getAllCoronaStats();
     this.getAllCoronaCountriesStats();
+
+    const map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
   }
 
   getAllCoronaStats() {
